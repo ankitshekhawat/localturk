6a0fb2a0ee43ce736a414e3aa9689046b3ae30ff
Ankit Shekhawat <contact@ankit.ws>
2017-12-18 23:40:23 +0530

    Added a very crude user auth method

--- localturk.ts
+++ localturk.ts
@@ -18,23 +18,23 @@ import open = require('open');
 import * as _ from 'lodash';
 
 import * as csv from './csv';
-import {makeTemplate} from './sample-template';
+import { makeTemplate } from './sample-template';
 import * as utils from './utils';
 import { outputFile } from 'fs-extra';
 
-import * as shuffleArray from  'shuffle-array';
+import * as shuffleArray from 'shuffle-array';
 program
   .version('2.0.3')
   .usage('[options] template.html tasks.csv outputs.csv')
   .option('-p, --port <n>', 'Run on this port (default 4321)', parseInt)
   .option('-s, --static-dir <dir>',
-          'Serve static content from this directory. Default is same directory as template file.')
+  'Serve static content from this directory. Default is same directory as template file.')
   .option('-w, --write-template', 'Generate a stub template file based on the input CSV.')
   .parse(process.argv);
 
-const {args, writeTemplate} = program;
+const { args, writeTemplate } = program;
 if (!((3 === args.length && !writeTemplate) ||
-     (1 === args.length && writeTemplate))) {
+  (1 === args.length && writeTemplate))) {
   program.help();
 }
 if (writeTemplate) {
@@ -49,11 +49,11 @@ const port = program.port || 4321;
 // temporary directory but the image files could be anywhere.
 const staticDir = program['staticDir'] || path.dirname(templateFile);
 
-type Task = {[key: string]: string};
+type Task = { [key: string]: string };
 let flash = '';  // this is used to show warnings in the web UI.
 
-async function renderTemplate({task, numCompleted, numTotal}: TaskStats) {
-  const template = await fs.readFile(templateFile, {encoding: 'utf8'});
+async function renderTemplate({ task, numCompleted, numTotal }: TaskStats, uid: String) {
+  const template = await fs.readFile(templateFile, { encoding: 'utf8' });
   const fullDict = {};
   for (const k in task) {
     fullDict[k] = utils.htmlEntities(task[k]);
@@ -65,20 +65,31 @@ async function renderTemplate({task, numCompleted, numTotal}: TaskStats) {
 
   const thisFlash = flash;
   flash = '';
-
   const sourceInputs = _.map(task, (v, k) =>
-      `<input type=hidden name="${k}" value="${utils.htmlEntities(v)}">`
-    ).join('\n');
+    `<input type=hidden name="${k}" value="${utils.htmlEntities(v)}">`
+  ).join('\n');
 
   return utils.dedent`
     <!doctype html>
     <html>
     <title>${numCompleted} / ${numTotal} - localturk</title>
     <body><form action=/submit method=post>
-    <p>${numCompleted} / ${numTotal} <span style="background: yellow">${thisFlash}</span></p>
+    <nav>
+    <div class="nav-wrapper">
+      <span class="brand-logo center">${numCompleted} / ${numTotal}</span>
+      <ul class="right hide-on-med-and-down">
+      <li><i class="material-icons left">account_circle</i>${uid}</li>
+      <li><a href="/"><i class="material-icons right">close</i></a></li>
+    </ul>
+    </div>
+  </nav>
+    <div class="container">
+    <p><span style="background: yellow">${thisFlash}</span></div>    
     ${sourceInputs}
     ${userHtml}
-    <hr/><input type=submit />
+    <hr/>
+    <input type="hidden" name="uid" value="${uid}"/>
+    <div class="container"><input class="waves-effect waves-light btn" type=submit value="submit" />  </div>
     </form>
     <script>
     // Support keyboard shortcuts via, e.g. <.. data-key="1" />
@@ -134,7 +145,7 @@ async function getNextTask(): Promise<TaskStats> {
   let taskList: Task[] = [];
   for await (const task of csv.readRowObjects(tasksFile)) {
     taskList.push(task);
-  }  
+  }
   for await (const task of shuffleArray(taskList)) {
     numTotal++;
     if (!nextTask && !isTaskCompleted(utils.normalizeValues(task), completedTasks)) {
@@ -150,15 +161,18 @@ async function getNextTask(): Promise<TaskStats> {
 }
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(errorhandler());
 app.use(express.static(path.resolve(staticDir)));
 
-app.get('/', utils.wrapPromise(async (req, res) => {
+app.get('/task', utils.wrapPromise(async (req, res) => {
+  let uid = req.query.uid;
+  if (typeof uid === 'undefined' || !uid || uid === '') { res.send('No User ID <a href="/">Go to login screen</a>') };
+  console.log(uid);
   const nextTask = await getNextTask();
   if (nextTask.task) {
     console.log(nextTask.task);
-    const html = await renderTemplate(nextTask);
+    const html = await renderTemplate(nextTask, uid);
     res.send(html);
   } else {
     res.send('DONE');
@@ -166,12 +180,24 @@ app.get('/', utils.wrapPromise(async (req, res) => {
   }
 }));
 
+app.get('/', utils.wrapPromise(async (req, res) => {
+  const html = await fs.readFile("login.html", { encoding: 'utf8' });
+  res.send(html);
+}));
+
 app.post('/submit', utils.wrapPromise(async (req, res) => {
   const task: Task = req.body;
   await csv.appendRow(outputsFile, task);
   checkTaskOutput(task);  // sets the "flash" variable with any errors.
   console.log('Saved ' + JSON.stringify(task));
-  res.redirect('/');
+  res.redirect('/task?uid=' + req.body.uid);
+}));
+
+app.post('/login-form', utils.wrapPromise(async (req, res) => {
+  for await (const user of csv.readRowObjects('meta/user_db.csv')) {
+    if (req.body.uid === user.uid) { res.redirect('/task?uid=' + req.body.uid); }
+  }
+  res.send('Invalid id, try again <a href="/">Go to login screen</a>');
 }));
 
 app.post('/delete-last', utils.wrapPromise(async (req, res) => {
